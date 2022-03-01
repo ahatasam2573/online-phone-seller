b5d654956a4b16f5f0b683aeb76f79f980e094ad
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-03-01 20:51:56 +0600

    final update

--- js/api.js
+++ js/api.js
@@ -18,19 +18,21 @@ const displaySearchResult = data => {
     if (data.length == 0) {
         const displayError = document.getElementById('error-message').style.display = 'block';
     }
-    else if (data.length <= 20) {
+    else {
         const searchResult = document.getElementById('search-result');
         searchResult.textContent = '';
-        (data.slice(0, 20)).forEach(info => {
+
+        //slicing phone list to 20
+        data.slice(0, 20).forEach(info => {
             // console.log(info);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-            <div class="card h-80 w-80 mx-auto mt-5">
-                    <img src="${info.image}" class="card-img-top" alt="...">
+            <div class="card h-80 w-80 mx-auto mt-5 rounded">
+                    <img src="${info.image}" class="card-img-top w-80 h-80 p-21" alt="...">
                     <div class="card-body">
                         <h4 class="card-title">${info.phone_name}</h4>
-                        <p class="card-text fw-bold">${info.brand}</p>
+                        <p class="card-text fw-bold fs-4">${info.brand}</p>
                         <button onclick="loadPhoneDetail('${info.slug}')" class="bg-success ps-2 pe-2 rounded text-white">Explore</button>
                     </div>
             </div>
@@ -38,9 +40,6 @@ const displaySearchResult = data => {
             searchResult.appendChild(div);
             document.getElementById('error-message').style.display = 'none';
         });
-    }
-    else {
-        const searchResult = document.getElementById('search-result').style.display = 'none';
     }
 
 }
@@ -61,7 +60,7 @@ const phoneDetail = info => {
     div.classList.add('card');
     div.innerHTML = `
         <img src="${info.image}" class="card-img-top w-100 h-100" alt="...">  
-        <div class="card-body h-300 w-300"                                                                                                                                                                                                                             ">
+        <div class="card-body h-300 w-300 px-auto rounded"                                                                                                                                                                                                                             ">
              <h5 class="card-title fw-bold">${info.name}</h5>
              <p class="card-text">ReleaseDate : ${info.releaseDate ? info.releaseDate : 'No relese date found!!'}</p>
              <p class="card-text">Sensors : ${info.mainFeatures.sensors}</p>
